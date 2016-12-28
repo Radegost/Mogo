37eb44aec01518e8dd6a6f705c773de4c3100300
Konrad Chojnecki <konrad.chojnecki@best.wroclaw.pl>
2016-12-28 22:04:30 +0100

    add nav menu close scrit

--- js/scrollAnimate.js
+++ js/scrollAnimate.js
@@ -2,6 +2,7 @@ $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
+            $('#myNavbar').removeClass("in");
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
                 $('html, body').animate({
@@ -9,6 +10,7 @@ $(function() {
                 }, 300).delay(700);
                 return false;
             }
+
         }
     });
 });
